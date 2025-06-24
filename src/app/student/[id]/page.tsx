4f15770cae0d8ad134dc79b12de5980a893ccea1
Unaiza Tariq <unaiza13tariq@gmail.com>
2025-06-24 18:58:26 +0500

    fix: corrected id type for Vercel build

--- src/app/student/[id]/page.tsx
+++ src/app/student/[id]/page.tsx
@@ -1,11 +1,8 @@
 import { getStudentById } from '@/app/actions/page'
-import { get } from 'http'
 import React from 'react'
 import {
   Card,
-  CardAction,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
@@ -14,35 +11,42 @@ import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 import { ArrowBigLeftIcon } from 'lucide-react'
 
-export default async function studentId({ params }: { params: { id: string } }) {
+// ✅ JUST THIS TYPE WAS ADDED
+type PageProps = {
+  params: {
+    id: string;
+  };
+};
+
+export default async function studentId({ params }: PageProps) {
   const { id } = params
-  const student= await getStudentById(Number(id))
+  const student = await getStudentById(Number(id))
+
   return (
     <div className='flex items-center justify-center h-screen'>
       <div className='p-5 bg-accent m-5 w-[400px]'>
-  <Card>
-  <CardHeader>
-    <CardTitle>Student ID: {student?.studentId}</CardTitle>
-  </CardHeader>
-  <CardContent>Student Name:{student?.firstName}{student?.lastName}</CardContent>
-  <CardContent>
-    <p>Status: {student?.isActive ? "yes" : "no"}</p>
-  </CardContent>
-  <CardFooter>
-    <p>Remarks: {student?.remarks}</p>
-  </CardFooter>
-<div className='flex justify-center'>
-  <Link href={`/student/list`}>
-  <Button asChild className='w-[330px]'>
-    <ArrowBigLeftIcon />
-  </Button>
-  </Link>
-  </div>
-</Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>Student ID: {student?.studentId}</CardTitle>
+          </CardHeader>
+          <CardContent>
+            Student Name: {student?.firstName} {student?.lastName}
+          </CardContent>
+          <CardContent>
+            <p>Status: {student?.isActive ? "yes" : "no"}</p>
+          </CardContent>
+          <CardFooter>
+            <p>Remarks: {student?.remarks}</p>
+          </CardFooter>
+          <div className='flex justify-center'>
+            <Link href={`/student/list`}>
+              <Button asChild className='w-[330px]'>
+                <ArrowBigLeftIcon />
+              </Button>
+            </Link>
+          </div>
+        </Card>
       </div>
-     
     </div>
-   
   )
 }
-
